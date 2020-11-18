7c1037663d84dde80fa3b535c2f70b3c6b1dedfe
sabamokhlesi <59446430+sabamokhlesi@users.noreply.github.com>
2020-11-17 19:37:13 -0500

    Updates

---
+++ precache-manifest.f16b5523b33673771fa957bef910c582.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32323dbe3bd045b6d2afcb0789609f1b",
+    "url": "/Cake-Website/index.html"
+  },
+  {
+    "revision": "e47f0eb1761bd44c26ab",
+    "url": "/Cake-Website/static/css/main.45cfc187.chunk.css"
+  },
+  {
+    "revision": "9bb2ead0d8793dd532ce",
+    "url": "/Cake-Website/static/js/2.3d86e251.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cake-Website/static/js/2.3d86e251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47f0eb1761bd44c26ab",
+    "url": "/Cake-Website/static/js/main.8c1d9e4d.chunk.js"
+  },
+  {
+    "revision": "73b08fbe97cebfc4d954",
+    "url": "/Cake-Website/static/js/runtime-main.7b109ed0.js"
+  },
+  {
+    "revision": "1932d8878c484d927496282ce03160b1",
+    "url": "/Cake-Website/static/media/feature2.1932d887.png"
+  },
+  {
+    "revision": "6b25e1ddb0d3c6e9c1a1d7401e29b9d9",
+    "url": "/Cake-Website/static/media/footer1.6b25e1dd.png"
+  },
+  {
+    "revision": "39b9c35cc61e26c01a2a56775b338c01",
+    "url": "/Cake-Website/static/media/footer2.39b9c35c.png"
+  },
+  {
+    "revision": "91c28638c7fe4e0a1735aa312dce9db0",
+    "url": "/Cake-Website/static/media/header-bg2.91c28638.jpg"
+  },
+  {
+    "revision": "da6e5c88fa0728b2a44ef185c8cc2b3d",
+    "url": "/Cake-Website/static/media/header2.da6e5c88.png"
+  },
+  {
+    "revision": "582ad5ff64370dc426ef41a1167f37f9",
+    "url": "/Cake-Website/static/media/menu-bg.582ad5ff.jpg"
+  },
+  {
+    "revision": "bddb010b329fa7491a4142568ce44283",
+    "url": "/Cake-Website/static/media/menu1.bddb010b.jpg"
+  },
+  {
+    "revision": "6ad70cda3ada7e76a973ce58ac3fd639",
+    "url": "/Cake-Website/static/media/menu2.6ad70cda.jpg"
+  },
+  {
+    "revision": "2512c9ad62b6e21d6dbdb79ed173c0e9",
+    "url": "/Cake-Website/static/media/menu3.2512c9ad.jpg"
+  },
+  {
+    "revision": "37680e2b295f6f85975f38284d10a5be",
+    "url": "/Cake-Website/static/media/steps-bg2.37680e2b.png"
+  },
+  {
+    "revision": "839ffb4de838a973f21bf1776635fd03",
+    "url": "/Cake-Website/static/media/story-bg4.839ffb4d.jpg"
+  },
+  {
+    "revision": "dbaeda3be4fd9a0e61721d9695fa70bb",
+    "url": "/Cake-Website/static/media/story.dbaeda3b.png"
+  }
+]);
